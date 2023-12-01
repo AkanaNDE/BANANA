3d3203773e42b4180c8aa81e87c4ace38547169b
AkanaNDE <Nackawat@gmail.com>
2023-12-01 16:10:00 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
+    int N;
+    int even = 0, odd = 0;
+while (N != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N;
+    if(N % 2 == 0){
+    even = even + 1;
+    }
+    else{
+    odd = odd +1;
+    }
+    }
+    even = even -1;
+    cout << "#Even numbers = " << even << "\n";
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
